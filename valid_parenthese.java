84a9ed95affee8a9aee086d5b8ad3487900b1733
Mengbabity <32949218+Mengbabity@users.noreply.github.com>
2017-12-23 14:07:35 +0800

    Create valid_parenthese.java

---
+++ valid_parenthese.java
@@ -0,0 +1,10 @@
+package valid_parenthese;
+
+public class valid_parenthese {
+public static void main(String[] args)
+{
+	solution s=new solution();
+	String strs="{[(())]}";
+	s.isValid(strs);
+}
+}
